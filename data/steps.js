8437ffac645f3ef299f1baafce67fa382e0e2802
RyanChan <ryanchanplc@gmail.com>
2021-04-19 07:39:50 +0800

    update Q/A and packages deps

--- data/steps.js
+++ data/steps.js
@@ -1,90 +1,92 @@
 import Links from '../components/Links'
-import {questions} from './questions'
+import { questions } from './questions'
 
-const removeAskedQuestion=(previousValue)=>{
-    questions.splice(
-        questions.findIndex(({ value }) => value == previousValue),
-        1
-      )
+const removeAskedQuestion = (previousValue) => {
+  questions.splice(
+    questions.findIndex(({ value }) => value == previousValue),
+    1
+  )
 }
 
 export const steps = [
-    {
-        id: 'Intro-1',
-        message: 'Hello! My name is Ryan',
-        trigger: 'Intro-2'
+  {
+    id: 'Intro-1',
+    message: 'Hello! My name is Ryan',
+    trigger: 'Intro-2',
+  },
+  {
+    id: 'Intro-2',
+    message: 'I am a Software Engineer.',
+    trigger: 'Intro-3',
+  },
+  {
+    id: 'Intro-3',
+    message: 'I have over 9 years software development experience.',
+    trigger: 'Questions',
+  },
+  {
+    id: 'Questions',
+    options: questions,
+  },
+  {
+    id: 'Education-1',
+    message: ({ previousValue }) => {
+      removeAskedQuestion(previousValue)
+      return 'I graduated from the Chinese University of Hong Kong'
     },
-    {
-        id: 'Intro-2',
-        message: 'I am a Software Engineer.',
-        trigger: 'Intro-3'
+    trigger: 'Education-2',
+  },
+  {
+    id: 'Education-2',
+    message: 'I have a M.Phil Degree in Mechanical and Automation Engineering',
+    trigger: 'Education-3',
+  },
+  {
+    id: 'Education-3',
+    message:
+      'and a Bacholer Degree in Automation and Computer Aided Engineering',
+    trigger: 'Questions',
+  },
+  {
+    id: 'Skill-1',
+    message: ({ previousValue }) => {
+      removeAskedQuestion(previousValue)
+      return 'I am specialized in developing front-end application like mobile ( Android / iOS / ReactNaive / Flutter ) and web applicaton ( React/Vue ). I am also familiar in backend development ( PHP / Nodejs )'
     },
-    {
-        id: 'Intro-3',
-        message: 'I have over 8 years software development experience.',
-        trigger: 'Questions'
+    trigger: 'Skill-2',
+  },
+  {
+    id: 'Skill-2',
+    message:
+      'I often write code in Typescript / Javascript / PHP / ObjectiveC / Java / C# / (some) Python',
+    trigger: 'Questions',
+  },
+  {
+    id: 'Work-1',
+    message: ({ previousValue }) => {
+      removeAskedQuestion(previousValue)
+      return 'Currently i am working for a fintech startup as a Front-end Engineer.'
     },
-    {
-        id: 'Questions',
-        options: questions
+    trigger: 'Work-2',
+  },
+  {
+    id: 'Work-2',
+    message:
+      'My responsibilities are revamp a legacy wealth management platform from Java GWT to React, and maintain/develop existing/new features',
+    trigger: 'Questions',
+  },
+  {
+    id: 'Links-1',
+    message: ({ previousValue }) => {
+      removeAskedQuestion(previousValue)
+      return 'Please check the links below : '
     },
-    {
-        id: 'Education-1',
-        message :({previousValue})=> {
-           removeAskedQuestion(previousValue);
-           return 'I graduated from the Chinese University of Hong Kong';
-        },
-        trigger: 'Education-2'
-    },
-    {
-        id: 'Education-2',
-        message: 'I have a M.Phil Degree in Mechanical and Automation Engineering',
-        trigger: 'Education-3'
-    },
-    {
-        id: 'Education-3',
-        message: 'and a Bacholer Degree in Automation and Computer Aided Engineering',
-        trigger: 'Questions'
-    },
-    {
-        id: 'Skill-1',
-        message: ({previousValue})=> {
-            removeAskedQuestion(previousValue);
-            return 'I am specialized in developing mobile ( Android / iOS / ReactNaive / Flutter ) and web applicaton ( PHP / ReactJS ).';
-        },
-        trigger: 'Skill-2'
-    },
-    {
-        id: 'Skill-2',
-        message: 'I often write code in Java / ObjectiveC / C# / Javascript / PHP / (some) Python',
-        trigger: 'Questions'
-    },
-    {
-        id: 'Work-1',
-        message: ({previousValue})=> {
-            removeAskedQuestion(previousValue);
-            return 'Currently i am working for a company to propose solutions based on user requirements and develop proof-of-concept application.';
-        },
-        trigger: 'Work-2'
-    },
-    {
-        id: 'Work-2',
-        message: 'I often need to learn new technologies including VR, AR, Blockchains and Deep Learning, etc',
-        trigger: 'Questions'
-    },
-    {
-        id: 'Links-1',
-        message: ({previousValue})=> {
-            removeAskedQuestion(previousValue);
-            return 'Please check the links below : ';
-        },
-        trigger: 'Links-2'
-    },
-    {
-        id: 'Links-2',
-        component: <Links />,
-        asMessage: true,
-        trigger: 'Questions'
-    }
-  ]
-  
+    trigger: 'Links-2',
+  },
+  {
+    id: 'Links-2',
+    component: <Links />,
+    asMessage: true,
+    trigger: 'Questions',
+  },
+]
